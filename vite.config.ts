eec1b590446d3e96b9129f3488a2c57819c12267
Chie Saito <qoo0123.chie0417@gmail.com>
2025-11-19 14:54:50 +0900

    依存関係インストール

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react()],
-  }
+export default defineConfig({
+  plugins: [react()],
 })
