63d4042f3511f5d0d7f8b697531a903537555c58
Chie Saito <qoo0123.chie0417@gmail.com>
2025-11-19 14:52:22 +0900

    API削除

--- vite-env.d.ts
+++
@@ -1,8 +0,0 @@
-// /// <reference types="vite/client" />
-
-declare const process: {
-  env: {
-    [key: string]: string | undefined;
-    API_KEY: string;
-  }
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import process from 'node:process'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
-    define: {
-      // Allows process.env.API_KEY to work in the client-side code
-      'process.env.API_KEY': JSON.stringify(env.API_KEY)
-    }
   }
 })
